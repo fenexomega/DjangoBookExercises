30751f63c2df1faff1a18eb1a8a8ba2c32df28ef
Jordy <jordyfgomes@gmail.com>
2014-08-04 16:55:52 -0300

    Poucas mudanças

--- djangobook/settings.py
+++ djangobook/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'example_app'
 )
 
@@ -70,7 +71,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
--- example_app/models.py
+++ example_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	name = models.CharField(max_length=50, verbose_name="Name")
+	login = models.CharField(max_length=30,verbose_name="Login")
+	password = models.CharField(max_length=30,verbose_name="Password")
+	phone = models.CharField(max_length=20,verbose_name="Login")
+	born_date = models.DateField(verbose_name="Born date", null=True,blank=True)
+	last_connection = models.DateTimeField(verbose_name="Date of last connection", null=True,blank=True,default=None)
+	email = models.EmailField(verbose_name="Email")
+	years_seniority = models.IntegerField(verbose_name="Seniority",default=0)
+	date_created = models.DateField(verbose_name="Date of Birthday")
+
+	
