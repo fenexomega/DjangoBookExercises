96f1c0a690fe01887886f4fff9ea434861cd18d7
Jordy <jordyfgomes@gmail.com>
2014-09-01 02:02:57 -0300

    Extending list view!

--- djangobook/urls.py
+++ djangobook/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import CreateView
+from example_app.models import Project,Task
+from django.views.generic.list import ListView
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +17,9 @@ urlpatterns = patterns('',
     url(r'^project-detail-(?P<pk>\d+)$','example_app.views.project_detail.page',name='project_detail'),
     url(r'^create_developer$','example_app.views.create_developer.page',name='create_developer'),
     url(r'^create_supervisor$','example_app.views.create_supervisor.page',name='create_supervisor'),
-    url(r'^create_project$','example_app.views.create_project.page',name='create_project')
+    # url(r'^create_project$','example_app.views.create_project.page',name='create_project'),
+    url(r'^create_project$',CreateView.as_view(model=Project, template_name="example_app/create_project.html",success_url='index'),name="create_project"),
+    url(r'^create_task$',CreateView.as_view(model=Task, template_name="example_app/create_task.html",success_url="index"),name="create_task"),
     #url(r'^connection$', 'example_app.views.connection.page', name="public_connection")
+    url(r'^project_list$',ListView.as_view(model=Project,template_name="example_app/project_detail.html"),name="project_detail")
 )
